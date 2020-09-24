fa12b44ac88a6df4b78e170f4cddaaf6eba7a889
luke <luke@lukes-MacBook-Pro.local>
2020-09-24 11:13:39 -0500

    adds new user signup

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,22 @@
 class Api::UsersController < ApplicationController
+  def create
+    user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      password: params[:password],
+      password_digest: params[:password_confirmation],
+      email: params[:email],
+      artist_name: params[:artist_name],
+      bio: params[:bio],
+      city: params[:city],
+      country: params[:country],
+      profile_picture: params[:profile_picture],
+      profile_url: params[:profile_url]
+    )
+    if user.save
+      render json: { "User created successfully"}, status: :created
+    else 
+      render json: { errors: user.errors.full_messages}, status: :bad_request
+    end 
+  end 
 end
